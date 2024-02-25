ad3ac9567adf155a5c0080aacd33813f21d3a4c1
otacoinkor <down80kg@gmail.com>
2024-02-25 16:33:05 +0900

    test

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 # main.py
 
 import streamlit as st
+import pandas as pd
+import pymysql
+import config
+
+
+st.set_page_config(
+    layout="wide",
+    page_title="Main")
 
 st.title("OTACO TEST")
+
+connection = pymysql.connect(
+    host=config.host,
+    user=config.user,
+    password=config.password,
+    database=config.database
+)
+
+print('connected')
